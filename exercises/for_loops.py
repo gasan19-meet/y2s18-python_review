a8df0054966fdde797c85405985ad99c764f78d9
Gasan Ashhab <gasanashhab@gmail.com>
2018-08-05 16:29:49 +0300

    kdf

--- exercises/for_loops.py
+++ exercises/for_loops.py
@@ -1,1 +1,26 @@
-# Write your solution for 1.1 here!
+# Write your solution for 1.1 here!f;
+# a=0
+a=0
+for i in range(1,101):
+	a+=i
+	print(a)
+# for i in range(2,100,2):
+
+# 	# a+=1
+# 	a+=i
+# 	print(a)
+# a=0
+# for i in range(1,101):
+# 	if i%2==0:
+# 		a+=i
+# 		print(a)
+# x=0
+# for i in range(1000):
+# 	if i%6==2 and(i**3)%5==3:
+# 		print(i)
+a=0
+while i<(10000):
+	i+=1
+	print(i)
+
+
